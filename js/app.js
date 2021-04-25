50a8034d27979d8aaf4747b42a8ada076b0cef33
GuzmanAndrew <ramirezandrew171@gmail.com>
2021-04-25 18:23:34 -0500

    Change Refactor front

--- js/app.js
+++ js/app.js
@@ -409,3 +409,19 @@ function initializeMap() {
 
     infowindow.open(map, marker);
 }
+
+function openDialog() {
+    $('#overlay').fadeIn('fast', function() {
+        $('#popup').css('display','block');
+        $('#popup').animate({'left':'30%'},500);
+    });
+}
+ 
+function closeDialog(id) {
+    $('#'+id).css('position','absolute');
+    $('#'+id).animate({'left':'-100%'}, 500, function() {
+        $('#'+id).css('position','fixed');
+        $('#'+id).css('left','100%');
+        $('#overlay').fadeOut('fast');
+    });
+}
